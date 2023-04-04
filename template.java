f4b48643fdbca8030cc9a97b92e6a549c2328252
Agustin Villa <58960825+agustin-villa@users.noreply.github.com>
2023-04-04 15:21:43 -0500

    Adding Template

---
+++ template.java
@@ -0,0 +1,11 @@
+
+//Class
+public class template {
+  
+    //Main Method
+    public static void main(String[] args) {
+    
+        //Print Statement 
+        System.out.println("Hello World!");
+  }
+}
