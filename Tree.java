0af730a2bc80cee789f458c692d02d20abe1df24
Agustin Villa <58960825+agustin-villa@users.noreply.github.com>
2023-04-04 16:23:49 -0500

    Adding Tree.java (Code Academy)

---
+++ Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+
+public class Tree{
+    public static void main(String[] args){
+  
+      //We will introduce ourselves and plant a tree 
+  
+      System.out.println("Agustin Villa");
+      System.out.println("    *    ");
+      System.out.println("   * *   ");
+      System.out.println("  * * *   ");
+      System.out.println("    *    ");
+    
+  
+    }
+  }
