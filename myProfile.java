21434db3301a018dc3437634eac6bf600d9f060f
Agustin Villa <58960825+agustin-villa@users.noreply.github.com>
2023-04-05 10:00:39 -0500

    Adding myProfile.java a Code Academy Review

---
+++ myProfile.java
@@ -0,0 +1,36 @@
+
+/* 
+Code Academy Review 
+
+The file MyProfile.java contains a class that represents your hiring profile as presented to potential employers.
+
+In the main() method, create a variable called name that holds your name, as a sequence of characters.
+Checkpoint 2 Passed
+2.
+
+Create a variable called age that holds your age as a whole number.
+Checkpoint 3 Passed
+3.
+
+Create a variable called desiredSalary that holds your desired salary per year to a precision of two decimal points.
+Checkpoint 4 Passed
+4.
+
+Create a variable called gender that holds a single character, m (male), f (female), n (for none), or o (for other).
+Checkpoint 5 Passed
+5.
+
+Create a variable called lookingForJob that holds whether or not you are currently open to job offers.
+
+*/
+
+public class MyProfile {
+	public static void main(String[] args) {   
+
+    String name  = "Agustin";
+    int age = 26;
+    double desiredSalary = 100000.65;
+    char gender = 'M';
+    boolean lookingForJob = false;
+	}
+}
