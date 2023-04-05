9c7d0a7699b314bc85b67faa05bfd344fd881529
Agustin Villa <58960825+agustin-villa@users.noreply.github.com>
2023-04-05 09:58:01 -0500

    Adding Variables to template

--- template.java
+++ template.java
@@ -7,5 +7,21 @@ public class template {
     
         //Print Statement 
         System.out.println("Hello World!");
-  }
+
+        //Integer Variable used to store whole numbers
+        int numbers = 26;
+
+        //Double Variable used to store decimals and large numbers
+        double decimals = 26.5;
+        
+        //Boolean used to store True or False
+        boolean bool = true;
+
+        //Char used to store single characters
+        char grade = 'A';
+
+        //String variable used to store text
+        String text = "This is a string";
+        
+    }
 }
