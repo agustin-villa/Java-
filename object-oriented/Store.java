6bced16c1ac2b7cc146a25908aac027c82a092eb
Agustin Villa <58960825+agustin-villa@users.noreply.github.com>
2023-04-09 14:54:09 -0500

    Create Store.java

---
+++ object-oriented/Store.java
@@ -0,0 +1,22 @@
+
+//Class
+public class Store {
+  // instance fields
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
+  
+  // constructor method
+  public Store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    //Instance of class store 
+    Store cookieShop = new Store("cookies", 12, 3.75);
+    
+  }
+}
